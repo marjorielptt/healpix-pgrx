e55bd7b966e16c43c1ee336ded0fe39aa668980c
M (✿◠‿◠) <marjorie.lapointe@zaclys.net>
2025-08-04 15:13:56 +0200

    Update tests.sql

--- pg_regress/sql/tests.sql
+++ pg_regress/sql/tests.sql
@@ -1,31 +1,18 @@
 -- TESTS
 
--- Creation of a MOC table
-CREATE TABLE moc_table(id bigint PRIMARY KEY, depth_max int4, ranges int8multirange);
-
--- Indexation of the ranges column
-CREATE INDEX ranges_idx ON moc_table USING GIST (ranges);
-
 -- Creation of a MOC
 SELECT create_range_moc_psql(29, ARRAY[int8range(100,200),int8range(300,400)]);
 
--- Insertion of MOCs in moc_table
-INSERT INTO moc_table (id, depth_max, ranges) VALUES (1, 29, int8multirange(int8range(100,200), int8range(300,400)));
-INSERT INTO moc_table (id, depth_max, ranges) VALUES (2, 28, int8multirange(int8range(500,600)));
-INSERT INTO moc_table (id, depth_max, ranges) VALUES (3, 27, int8multirange(int8range(150,250), int8range(350,450), int8range(550,650)));
-
--- Create a MOC from a row of moc_table
--- Make sure you created the function moc_from_moc_table() whose code is in ../setup.sql
-SELECT moc_from_moc_table(2);
-
 -- Function test : moc_to_ascii
 SELECT moc_to_ascii(create_range_moc_psql(29, ARRAY[int8range(100,200),int8range(300,400)]));
+
+-- Function test : moc_from_ascii_ivoa
 
 -- Function test : moc_and
 SELECT create_range_moc_psql(29, ARRAY[int8range(100,200),int8range(300,400)]) & create_range_moc_psql(28, ARRAY[int8range(500,600)]);
 
 -- Creation of a BMOC
-SELECT create_bmoc_psql(29, ARRAY[8202, 8203, 8206, 8207, 8218, 8224, 8225]);
+SELECT create_bmoc_psql(13, ARRAY[8202, 8203, 8206, 8218]) & create_bmoc_psql(13, ARRAY[8202, 8203, 8224, 8225]) AS intersection;
 
 -- Function test : hpx_elliptical_cone
 SELECT hpx_elliptical_cone_coverage(3, 36.80105218, 56.78028536, 14.93, 4.93, 75.0);
@@ -49,7 +36,7 @@ SELECT moc_from_cone(13.158329, -72.80028, 5.64323, 6, 5, 'All');
 SELECT * FROM hip_table WHERE hpx_hash_range(29, raicrs, deicrs) <@ to_ranges_moc_psql(moc_from_cone(13.158329, -72.80028, 5.64323, 6, 5, 'All'));
 
 -- Return the cells contained in the bmoc created from a cone
-explain select * from hip_table where hpx_hash_range(29, raicrs, deicrs) <@ to_ranges_bmoc_psql(hpx_cone_coverage_approx(29, 13.158329, -72.80028, 5.64323));
+select * from hip_table where hpx_hash_range(29, raicrs, deicrs) <@ to_ranges_bmoc_psql(hpx_cone_coverage_approx(29, 13.158329, -72.80028, 5.64323));
 
 -- Returns the cells COMPLETELY contained in the cone (flag=1)
 SELECT * FROM hip_table WHERE hpx_hash_range(29, raicrs, deicrs) <@ to_int8multirange(hpx_flag_one(create_bmoc_psql(29, ARRAY[8202, 8203, 8206, 8207, 8218, 8224, 8225])));
@@ -57,13 +44,140 @@ SELECT * FROM hip_table WHERE hpx_hash_range(29, raicrs, deicrs) <@ to_int8multi
 -- Returns the cells PARTIALLY contained in the cone (flag=0)
 SELECT * FROM hip_table WHERE hpx_hash_range(29, raicrs, deicrs) <@ to_int8multirange(hpx_flag_zero(create_bmoc_psql(29, ARRAY[8202, 8203, 8206, 8207, 8218, 8224, 8225])));
 
--- Only uses the index in the InitPlan1 but not everywhere
-SELECT * FROM hip_table h
-WHERE EXISTS (
-    SELECT 1 FROM hip_table h
+-- First test for is_in_cone_psql(...)
+SELECT * FROM is_in_cone_psql(
+    0.01814144, 3.94648893, 5.64323,
+    'hip_table', 29, 'raicrs', 'deicrs')
+AS t("HIP" bigint, "Vmag" double precision, "RAICRS" double precision, "DEICRS" double precision);
+
+-- Second test for is_in_cone_psql(...)
+SELECT * FROM is_in_cone_psql(
+    0.01814144, 3.94648893, 5.64323,
+    'tyc2', 29, 'ra_icrs_', 'de_icrs_')
+AS t("recno" integer,
+    "tyc1" smallint,
+    "tyc2" smallint,
+    "tyc3" smallint,
+    "pflag" character(1),
+    "ramdeg" double precision,
+    "demdeg" double precision,
+    "pmra" double precision,
+    "pmde" double precision,
+    "e_ramdeg" float,
+    "e_demdeg" float,
+    "e_pmra" float,
+    "e_pmde" float,
+    "epram" double precision,
+    "epdem" double precision,
+    "num" smallint,
+    "q_ramdeg" float,
+    "q_demdeg" float,
+    "q_pmra" float,
+    "q_pmde" float,
+    "btmag" float,
+    "e_btmag" float,
+    "vtmag" float,
+    "e_vtmag" float,
+    "prox" smallint,
+    "tyc" character(1),
+    "hip" integer,
+    "ccdm" character(3),
+    "ra_icrs_" double precision,
+    "de_icrs_" double precision,
+    "epra_1990" float,
+    "epde_1990" float,
+    "e_radeg" float,
+    "e_dedeg" float,
+    "posflg" character(1),
+    "corr" float);
+
+-- Test function in which we declare a bmoc that is not in the parameters
+create or replace function test_declare() returns text as $$
+declare
+    bmoc bmocpsql;
+BEGIN
+    bmoc:=create_bmoc_psql(13, ARRAY[8202, 8203, 8206, 8218]);
+    execute(SELECT * FROM hip_table h
     WHERE
-        hpx_hash_range(29, h.raicrs, h.deicrs) <@ to_int8multirange(hpx_flag_one(hpx_cone_coverage_approx(6, 13.158329, -72.80028, 5.64323))))
-    OR 
-        ((hpx_hash_range(29, h.raicrs, h.deicrs) <@ to_int8multirange(hpx_flag_zero(hpx_cone_coverage_approx(6, 13.158329, -72.80028, 5.64323))))
-        AND (hpx_contains_bool(hpx_cone_coverage_approx(6, 13.158329, -72.80028, 5.64323),h.raicrs, h.deicrs))
+        (hpx_hash_range(29, h.raicrs, h.deicrs) <@ to_int8multirange(hpx_flag_one(hpx_cone_coverage_approx(hpx_best_starting_depth(radius)+4, lon, lat, radius))))
+    OR
+        ((hpx_hash_range(29, h.raicrs, h.deicrs) <@ to_int8multirange(hpx_flag_zero(hpx_cone_coverage_approx(hpx_best_starting_depth(radius)+4, lon, lat, radius))))
+        AND
+        (hpx_contains_bool(hpx_cone_coverage_approx(hpx_best_starting_depth(radius)+4, lon, lat, radius), h.raicrs, h.deicrs)))
+    ) as res;
+    return res;
+end;
+$$ language plpgsql immutable
+;
+
+-- Query inside is_in_cone_psql
+SELECT * FROM hip_table
+WHERE
+(
+    hpx_hash_range(29, raicrs, deicrs)
+    <@
+    to_int8multirange(
+        hpx_flag_one(
+            hpx_cone_coverage_approx(
+                hpx_best_starting_depth(5.64323)+4, 0.01814144, 3.94648893, 5.64323
+            )
+        )
+    )
+)
+OR
+(
+    (
+        hpx_hash_range(29, raicrs, deicrs)
+        <@
+        to_int8multirange(
+            hpx_flag_zero(
+                hpx_cone_coverage_approx(
+                    hpx_best_starting_depth(5.64323)+4, 0.01814144, 3.94648893, 5.64323
+                )
+            )
+        )
+    )
+    AND
+    (
+        hpx_contains_bool(
+            hpx_cone_coverage_approx(
+                hpx_best_starting_depth(5.64323)+4, 0.01814144, 3.94648893, 5.64323
+            ),
+            raicrs,
+            deicrs
+        )
+    )
+);
+
+-- Query that needs skyregions'function contains
+SELECT * FROM hip_table
+WHERE
+(
+    hpx_hash_range(29, raicrs, deicrs)
+    <@
+    to_int8multirange(
+        hpx_flag_one(
+            hpx_cone_coverage_approx(
+                hpx_best_starting_depth(5.64323)+4, 0.01814144, 3.94648893, 5.64323
+            )
+        )
+    )
+)
+OR
+(
+    (
+        hpx_hash_range(29, raicrs, deicrs)
+        <@
+        to_int8multirange(
+            hpx_flag_zero(
+                hpx_cone_coverage_approx(
+                    hpx_best_starting_depth(5.64323)+4, 0.01814144, 3.94648893, 5.64323
+                )
+            )
+        )
+    )
+    AND
+    (
+        skyregion_cone_contains(0.01814144, 3.94648893, 5.64323, raicrs, deicrs)
+    )
 );
